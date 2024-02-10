bac4bae0c877665cfba5dbf68442ddba5b5d8ed4
320948-AAPS <320948@aaps.k12.mi.us>
2024-02-10 13:06:09 -0500

    Auton

--- robotconfig.py
+++ robotconfig.py
@@ -162,7 +162,7 @@ autonConfig = {
     'DO_COMMUNITY': False, # Only applies for position B
     'AUTON_OPEN_LOOP_RAMP_RATE': 1, # Improves the quality of swervometery by avoiding slippage.
     'AUTON_CLOSED_LOOP_RAMP_RATE': 0,
-    'TASK_RED_A': [['WAIT', 2], ['MOVE', 134.625, 16.75, 180], ['WAIT', 2], ['MOVE', 248.625, 16.75, 180], ['WAIT', 2], ['MOVE', 134.625, 73.75, 180], ['WAIT', 2], ['MOVE', 248.625, 16.75, 180]] #,['MOVE', 91.9375, -40.15, 180]]
+    'TASK_RED_A': [['MOVE', 188.625, 16.75, 0]]
     # 'TASK_BLU_A_TF': [['CLAW_INTAKE_AND_STOP'],
     #                     ['POSITION_GRABBER', 2],
     #                     ['ELEVATOR_DOWN'],
